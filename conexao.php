6d7d1bbdcb4a7918936a1754f00eb3f018e411ce
BrunoDuraess <brunoduraes03@gmail.com>
2024-08-14 15:59:24 -0300

    update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,3 +6,15 @@ $dbName = 'db name';
 
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+$createTable = "CREATE TABLE `api_cotacao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user` varchar(50) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `request` text NOT NULL,
+  `response` text NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `error_message` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;";
